a1358e0092aba120fd9c56d23bc37e090dbc54d3
Mohamed Amine <medamineboufares@gmail.com>
2022-10-22 10:32:44 +0100

    added header

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Header from "../components/Header/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Medium clone</title>
+      </Head>
+
+      <Header />
+    </div>
+  );
+}
