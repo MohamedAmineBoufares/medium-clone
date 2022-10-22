17e5a48533cee76b8b192c97c9f8e5561fbc4e97
Mohamed Amine <medamineboufares@gmail.com>
2022-10-22 13:06:54 +0100

    finished Post card styling and rendering

--- components/Posts/PostCard.tsx
+++ components/Posts/PostCard.tsx
@@ -4,13 +4,19 @@ import { urlFor } from "../../sanity";
 function PostCard({ slug, mainImage, title, description, author }) {
   return (
     <Link href={`/post/${slug.current}`}>
-      <div>
-        <img src={urlFor(mainImage).url()!} title={title} alt={title} />
+      <div className="group cursor-pointer border rounded-lg overflow-hidden">
+        <img
+          className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+          src={urlFor(mainImage).url()!}
+          title={title}
+          alt={title}
+        />
 
         <div className="flex justify-between p-5 bg-white">
           <div>
-            <p>{title}</p>
-            <p>
+            <p className="text-lg font-bold">{title}</p>
+
+            <p className="text-xs">
               {description} by {author.name}
             </p>
           </div>
